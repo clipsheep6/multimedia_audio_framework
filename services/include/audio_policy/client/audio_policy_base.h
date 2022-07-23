807d186ec0c9e68073a732445019bd034c45528d
boxwall <hezhiqiang19@huawei.com>
2022-07-23 15:56:03 +0800

    enable multi-sink filter:{uid,streamtype,streamid} devices:vector<device> enable move sink-input modify get all sink-input add get all sinks
    Signed-off-by: boxwall <hezhiqiang19@huawei.com>
    Change-Id: I92df56cd69c0875b8ff76b035c2c16ced10bc569

--- services/include/audio_policy/client/audio_policy_base.h
+++ services/include/audio_policy/client/audio_policy_base.h
@@ -122,6 +122,9 @@ public:
     virtual int32_t GetCurrentCapturerChangeInfos(
         std::vector<std::unique_ptr<AudioCapturerChangeInfo>> &audioCapturerChangeInfos) = 0;
 
+    virtual int32_t SelectOutputDevice(sptr<AudioRendererFilter> audioRendererFilter, std::vector<sptr<AudioDeviceDescriptor>> audioDeviceDescriptors) = 0;
+
+    virtual int32_t SelectIntputDevice(sptr<AudioCapturerFilter> audioCapturerFilter, std::vector<sptr<AudioDeviceDescriptor>> audioDeviceDescriptors) = 0;
 public:
     DECLARE_INTERFACE_DESCRIPTOR(u"IAudioPolicy");
 };
