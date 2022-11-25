8984c22c0dcd48fa79931b52ac674fb6b621f98b
jiao_yanlin <jiaoyanlin@huawei.com>
2022-11-25 08:55:04 +0800

    Sample code modification
    Signed-off-by: jiao_yanlin <jiaoyanlin@huawei.com>

--- frameworks/js/napi/audio_manager/src/audio_manager_napi.cpp
+++ frameworks/js/napi/audio_manager/src/audio_manager_napi.cpp
@@ -1163,7 +1163,7 @@ napi_value AudioManagerNapi::SetAudioScene(napi_env env, napi_callback_info info
 
             if (i == PARAM0 && valueType == napi_number) {
                 napi_get_value_int32(env, argv[i], &asyncContext->scene);
-                if ((asyncContext->scene >= AUDIO_SCENE_DEFAULT) || (asyncContext->scene <= AUDIO_SCENE_PHONE_CHAT)) {
+                if ((asyncContext->scene >= AUDIO_SCENE_DEFAULT) && (asyncContext->scene <= AUDIO_SCENE_PHONE_CHAT)) {
                     break;    
                 }
                 asyncContext->status = NAPI_ERR_UNSUPPORTED;
