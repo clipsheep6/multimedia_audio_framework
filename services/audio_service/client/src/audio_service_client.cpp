16444dcde593794ce77d5492eb5c2071420b24db
huangqiang <huangqiang73@huawei.com>
2023-08-17 20:45:16 +0800

    fix GetSessionID crash
    Signed-off-by: huangqiang <huangqiang73@huawei.com>

--- services/audio_service/client/src/audio_service_client.cpp
+++ services/audio_service/client/src/audio_service_client.cpp
@@ -989,7 +989,8 @@ int32_t AudioServiceClient::CreateStream(AudioStreamParams audioParams, AudioStr
     pa_proplist_sets(propList, "stream.type", streamName.c_str());
     pa_proplist_sets(propList, "stream.volumeFactor", std::to_string(mVolumeFactor).c_str());
     pa_proplist_sets(propList, "stream.powerVolumeFactor", std::to_string(mPowerVolumeFactor).c_str());
-    pa_proplist_sets(propList, "stream.sessionID", std::to_string(pa_context_get_index(context)).c_str());
+    sessionID = pa_context_get_index(context);
+    pa_proplist_sets(propList, "stream.sessionID", std::to_string(sessionID).c_str());
     pa_proplist_sets(propList, "stream.startTime", streamStartTime.c_str());
 
     if (eAudioClientType == AUDIO_SERVICE_CLIENT_RECORD) {
@@ -1091,22 +1092,12 @@ uint32_t AudioServiceClient::GetUnderflowCount()
 
 int32_t AudioServiceClient::GetSessionID(uint32_t &sessionID) const
 {
-    AUDIO_DEBUG_LOG("GetSessionID");
-    if (CheckPaStatusIfinvalid(mainLoop, context, paStream, AUDIO_CLIENT_PA_ERR) < 0) {
-        AUDIO_ERR_LOG("GetSessionID failed, pa_status is invalid");
-        return AUDIO_CLIENT_PA_ERR;
+    AUDIO_DEBUG_LOG("GetSessionID %{public}d", sessionID);
+    if (sessionID != PA_INVALID_INDEX && sessionID != 0) {
+        sessionID = this->sessionID;
+        return AUDIO_CLIENT_SUCCESS;
     }
-    pa_threaded_mainloop_lock(mainLoop);
-    uint32_t client_index = pa_context_get_index(context);
-    pa_threaded_mainloop_unlock(mainLoop);
-    if (client_index == PA_INVALID_INDEX) {
-        AUDIO_ERR_LOG("GetSessionID failed, sessionID is invalid");
-        return AUDIO_CLIENT_ERR;
-    }
-
-    sessionID = client_index;
-
-    return AUDIO_CLIENT_SUCCESS;
+    return AUDIO_CLIENT_ERR;
 }
 
 int32_t AudioServiceClient::StartStream(StateChangeCmdType cmdType)
