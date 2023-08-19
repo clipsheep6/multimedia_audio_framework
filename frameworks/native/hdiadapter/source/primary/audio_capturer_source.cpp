679da074da42c8629990d48f00aecd9d70eb51c5
yezizhuo <yezizhuo@huawei.com>
2023-08-19 12:45:18 +0800

    蓝牙、播放、录音支持para开启dump
    Signed-off-by: yezizhuo <yezizhuo@huawei.com>

--- frameworks/native/hdiadapter/source/primary/audio_capturer_source.cpp
+++ frameworks/native/hdiadapter/source/primary/audio_capturer_source.cpp
@@ -70,7 +70,7 @@ private:
     int32_t CreateCapture(struct AudioPort &capturePort);
     int32_t InitAudioManager();
     void InitAttrsCapture(struct AudioSampleAttributes &attrs);
-    void OpenDumpFile();
+    void SetAudioDumpBySysParam();
 
     IAudioSourceAttr attr_;
     bool capturerInited_;
@@ -97,9 +97,9 @@ private:
 
     IAudioSourceCallback* audioCapturerSourceCallback_ = nullptr;
     std::mutex audioCapturerSourceCallbackMutex_;
-#ifdef CAPTURE_DUMP
-    FILE *pfd_;
-#endif
+    bool enableDump_ = false;
+    FILE *dumpFile_ = nullptr;
+    uint32_t dumpCount = 0;
 };
 
 class AudioCapturerSourceWakeup : public AudioCapturerSource {
@@ -255,9 +255,6 @@ private:
     static inline AudioCapturerSourceInner audioCapturerSource_;
 };
 
-#ifdef CAPTURE_DUMP
-const char *g_audioOutTestFilePath = "/data/data/.pulse_dir/dump_audiosource.pcm";
-#endif // CAPTURE_DUMP
 bool AudioCapturerSource::micMuteState_ = false;
 constexpr int32_t RUNNINGLOCK_LOCK_TIMEOUTMS_LASTING = -1;
 
@@ -267,9 +264,6 @@ AudioCapturerSourceInner::AudioCapturerSourceInner(const std::string halName)
       audioCapture_(nullptr), halName_(halName)
 {
     attr_ = {};
-#ifdef CAPTURE_DUMP
-    pfd_ = nullptr;
-#endif // CAPTURE_DUMP
 }
 
 AudioCapturerSourceInner::~AudioCapturerSourceInner()
@@ -374,12 +368,10 @@ void AudioCapturerSourceInner::DeInit()
     }
     audioAdapter_ = nullptr;
     audioManager_ = nullptr;
-#ifdef CAPTURE_DUMP
-    if (pfd_) {
-        fclose(pfd_);
-        pfd_ = nullptr;
+    if (dumpFile_) {
+        fclose(dumpFile_);
+        dumpFile_ = nullptr;
     }
-#endif // CAPTURE_DUMP
 }
 
 void AudioCapturerSourceInner::InitAttrsCapture(struct AudioSampleAttributes &attrs)
@@ -526,18 +518,7 @@ int32_t AudioCapturerSourceInner::Init(IAudioSourceAttr &attr)
     }
     capturerInited_ = true;
 
-    OpenDumpFile();
     return SUCCESS;
-}
-
-void AudioCapturerSourceInner::OpenDumpFile()
-{
-#ifdef CAPTURE_DUMP
-    pfd_ = fopen(g_audioOutTestFilePath, "wb+");
-    if (pfd_ == nullptr) {
-        AUDIO_ERR_LOG("Error opening pcm test file!");
-    }
-#endif // CAPTURE_DUMP
 }
 
 int32_t AudioCapturerSourceInner::CaptureFrame(char *frame, uint64_t requestBytes, uint64_t &replyBytes)
@@ -556,14 +537,12 @@ int32_t AudioCapturerSourceInner::CaptureFrame(char *frame, uint64_t requestByte
         return ERR_READ_FAILED;
     }
 
-#ifdef CAPTURE_DUMP
-    if (pfd_) {
-        size_t writeResult = fwrite(frame, 1, replyBytes, pfd_);
+    if (dumpFile_) {
+        size_t writeResult = fwrite(frame, 1, replyBytes, dumpFile_);
         if (writeResult != replyBytes) {
             AUDIO_ERR_LOG("Failed to write the file.");
         }
     }
-#endif // CAPTURE_DUMP
 
     stamp = (ClockTime::GetCurNano() - stamp) / AUDIO_US_PER_SECOND;
     AUDIO_DEBUG_LOG("RenderFrame len[%{public}" PRIu64 "] cost[%{public}" PRId64 "]ms", requestBytes, stamp);
@@ -590,6 +569,18 @@ int32_t AudioCapturerSourceInner::Start(void)
         keepRunningLock_->Lock(RUNNINGLOCK_LOCK_TIMEOUTMS_LASTING); // -1 for lasting.
     } else {
         AUDIO_ERR_LOG("keepRunningLock_ is null, start can not work well!");
+    }
+
+    SetAudioDumpBySysParam();
+    if (enableDump_ && dumpFile_ == nullptr) {
+        std::string dumpFilePath = "/data/data/.pulse_dir/dump_audiocapture_" +
+        std::to_string(dumpCount) + ".pcm";
+        dumpFile_ = fopen(dumpFilePath.c_str(), "wb+");
+        if (dumpFile_ == nullptr) {
+            AUDIO_ERR_LOG("Error opening dump file!");
+        } else {
+            dumpCount++;
+        }
     }
 
     int32_t ret;
@@ -1084,5 +1075,28 @@ void AudioCapturerSourceWakeup::RegisterAudioCapturerSourceCallback(IAudioSource
     audioCapturerSource_.RegisterAudioCapturerSourceCallback(callback);
 }
 
+void AudioCapturerSourceInner::SetAudioDumpBySysParam(void)
+{
+    std::string dumpEnable;
+    enableDump_ = false;
+    bool res = GetSysPara("sys.audio.dump.writehdi.enable", dumpEnable);
+    if (!res || dumpEnable.empty()) {
+        AUDIO_INFO_LOG("sys.audio.dump.writehdi.enable is not set, dump audio is not required");
+        if (dumpFile_) {
+            fclose(dumpFile_);
+            dumpFile_ = nullptr;
+        }
+        return;
+    }
+    AUDIO_INFO_LOG("sys.audio.dump.writehdi.enable=%s", dumpEnable.c_str());
+    if (dumpEnable == "true") {
+        enableDump_ = true;
+        return;
+    }
+    if (dumpFile_) {
+        fclose(dumpFile_);
+        dumpFile_ = nullptr;
+    }
+}
 } // namespace AudioStandard
 } // namesapce OHOS
