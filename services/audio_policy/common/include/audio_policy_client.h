4a143f53467b74fc97e5399c1a05d5e9f2e4230c
liuyuehua1 <liuyuehua1@huawei.com>
2023-10-13 10:20:23 +0800

    modify volume callback register.
    Signed-off-by: liuyuehua1 <liuyuehua1@huawei.com>

--- services/audio_policy/common/include/audio_policy_client.h
+++ services/audio_policy/common/include/audio_policy_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef ST_AUDIO_VOLUME_KEY_EVENT_CALLBACK_H
-#define ST_AUDIO_VOLUME_KEY_EVENT_CALLBACK_H
+#ifndef ST_AUDIO_POLICY_CLIENT_H
+#define ST_AUDIO_POLICY_CLIENT_H
 
 #include "ipc_types.h"
 #include "iremote_broker.h"
@@ -24,15 +24,17 @@
 
 namespace OHOS {
 namespace AudioStandard {
-class IAudioVolumeKeyEventCallback : public IRemoteBroker {
+static const int32_t UPDATE_POLICY_CALLBACK_CLIENT = 0;
+enum class AudioPolicyClientCode {
+    ON_VOLUME_KEY_EVENT = 0,
+    AUDIO_POLICY_CLIENT_CODE_MAX = ON_VOLUME_KEY_EVENT,
+};
+class IAudioPolicyClient : public IRemoteBroker {
 public:
     virtual void OnVolumeKeyEvent(VolumeEvent volumeEvent) = 0;
-    enum {
-        ON_VOLUME_KEY_EVENT = 0,
-    };
 public:
-    DECLARE_INTERFACE_DESCRIPTOR(u"IAudioVolumeKeyEventCallback");
+    DECLARE_INTERFACE_DESCRIPTOR(u"IAudioPolicyClient");
 };
 } // namespace AudioStandard
 } // namespace OHOS
-#endif // ST_AUDIO_VOLUME_KEY_EVENT_CALLBACK_H
+#endif // ST_AUDIO_POLICY_CLIENT_H
