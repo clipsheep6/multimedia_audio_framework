65095f8f3e7431b2c1b81f79807d82dca0f34409
guxuewei123 <guxuewei123@huawei.com>
2023-10-28 20:11:25 +0800

    audio device manager
    Signed-off-by: guxuewei123 <guxuewei123@huawei.com>

--- services/audio_policy/server/include/service/audio_policy_service.h
+++ services/audio_policy/server/include/service/audio_policy_service.h
@@ -44,6 +44,8 @@
 #include "audio_effect_manager.h"
 #include "audio_volume_config.h"
 #include "policy_provider_stub.h"
+#include "audio_device_manager.h"
+#include "audio_device_parser.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -314,7 +316,8 @@ private:
         :audioPolicyManager_(AudioPolicyManagerFactory::GetAudioPolicyManager()),
         configParser_(ParserFactory::GetInstance().CreateParser(*this)),
         streamCollector_(AudioStreamCollector::GetAudioStreamCollector()),
-        audioEffectManager_(AudioEffectManager::GetAudioEffectManager())
+        audioEffectManager_(AudioEffectManager::GetAudioEffectManager()),
+        audioDeviceManager_(AudioDeviceManager::GetAudioDeviceManager())
     {
 #ifdef ACCESSIBILITY_ENABLE
         accessibilityConfigListener_ = std::make_shared<AccessibilityConfigListener>(*this);
@@ -591,6 +594,7 @@ private:
     std::mutex microphonesMutex_;
 
     bool isArmUsbDevice_ = false;
+    AudioDeviceManager &audioDeviceManager_;
 };
 } // namespace AudioStandard
 } // namespace OHOS
