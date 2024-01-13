1e3e008623c8d7206a741bc73228fd65dd700092
dlluhj <dailuhan@h-partners.com>
2024-01-13 07:23:36 +0000

    update services/audio_service/server/src/audio_server.cpp.
    Signed-off-by: dlluhj <dailuhan@h-partners.com>

--- services/audio_service/server/src/audio_server.cpp
+++ services/audio_service/server/src/audio_server.cpp
@@ -569,10 +569,14 @@ sptr<IRemoteObject> AudioServer::CreateAudioProcess(const AudioProcessConfig &co
         resetConfig.appInfo.appTokenId = IPCSkeleton::GetCallingTokenID();
     }
 
-    // check MICROPHONE_PERMISSION
-    bool res = VerifyClientPermission(MICROPHONE_PERMISSION, resetConfig.appInfo.appTokenId);
-    CHECK_AND_RETURN_RET_LOG(config.audioMode != AUDIO_MODE_RECORD || res, nullptr,
-        "CreateAudioProcess for record failed:No permission.");
+    // check MICROPHONE_PERMISSIONconstexpr uid_t UID_FOUNDATION_SA = 5523;
+    if (resetConfig.capturerInfo.sourceType == SOURCE_TYPE_VIRTUAL_CAPTURE && callerUid == UID_FOUNDATION_SA) {
+        AUIDO_INFO_LOG("sourcetype is virtual capture");
+    } else {
+        bool res = VerifyClientPermission(MICROPHONE_PERMISSION, resetConfig.appInfo.appTokenId);
+        CHECK_AND_RETURN_RET_LOG(config.audioMode != AUDIO_MODE_RECORD || res, nullptr,
+            "CreateAudioProcess for record failed:No permission.");
+    }
 
     // Check MANAGE_INTELLIGENT_VOICE_PERMISSION and system permission
     if (resetConfig.isWakeupCapturer == true) {
