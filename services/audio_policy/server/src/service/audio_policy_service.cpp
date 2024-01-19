af0ee37aa8a17d3c4f0ec3d404260230201e3283
dlluhj <dailuhan@h-partners.com>
2024-01-19 08:55:37 +0000

    修复畅联消息选择使用听筒播放，暂停两次之后再次播放从扬声器出声
    Signed-off-by: dlluhj <dailuhan@h-partners.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -1706,13 +1706,14 @@ void AudioPolicyService::FetchOutputDevice(vector<unique_ptr<AudioRendererChange
         runningStreamCount++;
         unique_ptr<AudioDeviceDescriptor> desc = audioRouterCenter_.FetchOutputDevice(
             rendererChangeInfo->rendererInfo.streamUsage, rendererChangeInfo->clientUID);
-        unique_ptr<AudioDeviceDescriptor> currentActiveDevice = make_unique<AudioDeviceDescriptor>(currentActiveDevice_);
+        AudioDeviceDescriptor currentActiveDevice = currentActiveDevice_;
         DeviceInfo outputDeviceInfo = rendererChangeInfo->outputDeviceInfo;
         if (desc->deviceType_ == DEVICE_TYPE_NONE || (IsSameDevice(desc, outputDeviceInfo) &&
             !NeedRehandleA2DPDevice(desc) && !sameDeviceSwitchFlag_)) {
             AUDIO_INFO_LOG("stream %{public}d device not change, no need move device", rendererChangeInfo->sessionId);
             if (!IsSameDevice(desc, currentActiveDevice)) {
                 currentActiveDevice_ = AudioDeviceDescriptor(*desc);
+                AUDIO_DEBUG_LOG("currentActiveDevice update %{public}d", currentActiveDevice.deviceType_);
             }
             continue;
         }
