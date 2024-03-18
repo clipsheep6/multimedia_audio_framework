97d4ae9a4015b440b8ee78d00fa3dbe00ab080b7
刘乐 <liule50@huawei.com>
2024-03-18 10:43:30 +0000

    Solve line width 120 characters
    Signed-off-by: 刘乐 <liule50@huawei.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -2025,7 +2025,8 @@ int32_t AudioPolicyService::SelectNewDevice(DeviceRole deviceRole, const sptr<Au
     return SUCCESS;
 }
 
-void AudioPolicyService::UpdatePreferredDevice(DeviceRole deviceRole, const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+void AudioPolicyService::UpdatePreferredDevice(DeviceRole deviceRole, 
+    const sptr<AudioDeviceDescriptor> &deviceDescriptor)
 {
     if (deviceRole == DeviceRole::OUTPUT_DEVICE) {
         if (GetVolumeGroupType(currentActiveDevice_.deviceType_) != GetVolumeGroupType(deviceDescriptor->deviceType_)) {
