d3e7c0a942e2fa9ef18550f078e47b36b156a71b
Myy_gitee <206596337@qq.com>
2024-04-15 19:36:49 +0800

    test

--- interfaces/inner_api/native/audiocommon/include/audio_device_info.h
+++ interfaces/inner_api/native/audiocommon/include/audio_device_info.h
@@ -139,13 +139,9 @@ enum DeviceType {
      */
     DEVICE_TYPE_USB_HEADSET = 22,
     /**
-     * Indicates a display device.
-     */
-    DEVICE_TYPE_DP = 23,
-    /**
      * Indicates a usb-arm device.
      */
-    DEVICE_TYPE_USB_ARM_HEADSET = 24,
+    DEVICE_TYPE_USB_ARM_HEADSET = 23,
     /**
      * Indicates a debug sink device
      */
@@ -191,7 +187,6 @@ inline const std::unordered_set<DeviceType> OUTPUT_DEVICE_TYPE_SET = {
     DeviceType::DEVICE_TYPE_BLUETOOTH_SCO,
     DeviceType::DEVICE_TYPE_BLUETOOTH_A2DP,
     DeviceType::DEVICE_TYPE_USB_HEADSET,
-    DeviceType::DEVICE_TYPE_DP,
     DeviceType::DEVICE_TYPE_USB_ARM_HEADSET,
     DeviceType::DEVICE_TYPE_FILE_SINK,
 };
