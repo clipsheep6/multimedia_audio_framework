5beddbc3b5926989c542a374a6f3ed33ab2dd820
Jvgang <yujugang1@h-partners.com>
2024-08-09 06:29:46 +0000

    处理门禁提示SetAudioStreamInfo 超过50行是超大函数问题
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- services/audio_service/client/src/renderer_in_client.cpp
+++ services/audio_service/client/src/renderer_in_client.cpp
@@ -312,10 +312,9 @@ int32_t RendererInClientInner::SetAudioStreamInfo(const AudioStreamParams info,
     if (rendererInfo_.rendererFlags == AUDIO_FLAG_VOIP_DIRECT || IsHighResolution()) {
         int32_t type = ipcStream_->GetStreamManagerType();
         if (type == AUDIO_DIRECT_MANAGER_TYPE) {
-            std::unique_lock<std::mutex> pipeTypeLock(g_pipeTypeMutex);
+            std::lock_guard<std::mutex> pipeTypeLock(g_pipeTypeMutex);
             rendererInfo_.pipeType = (rendererInfo_.rendererFlags == AUDIO_FLAG_VOIP_DIRECT) ?
                 PIPE_TYPE_DIRECT_VOIP : PIPE_TYPE_DIRECT_MUSIC;
-            pipeTypeLock.unlock();
         }
     }
     proxyObj_ = proxyObj;
