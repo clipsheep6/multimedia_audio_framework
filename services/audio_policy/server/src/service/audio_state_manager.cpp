2b0fb316aff11ecb40995611dc529ef63250aeb3
littleBoySmile <yangjun265@h-partners.com>
2024-08-25 14:58:57 +0000

    add client support in audioStateManager
    Change-Id: I94d0526a12f98ecb9d0fe40b5555ee46a6ee1eb7

--- services/audio_policy/server/src/service/audio_state_manager.cpp
+++ services/audio_policy/server/src/service/audio_state_manager.cpp
@@ -21,70 +21,190 @@ using namespace std;
 namespace OHOS {
 namespace AudioStandard {
 
-void AudioStateManager::SetPreferredMediaRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+static int32_t PREFERRED_MEDIA_RENDER = 1 << 0;
+static int32_t PREFERRED_CALL_RENDER = 1 << 1;
+static int32_t PREFERRED_CALL_CAPTURE = 1 << 2;
+static int32_t PREFERRED_RING_RENDER = 1 << 3;
+static int32_t PREFERRED_RECORD_CAPTURE = 1 << 4;
+static int32_t PREFERRED_TONE_RENDER = 1 << 4;
+
+void AudioStateManager::SetPreferredMediaRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
     preferredMediaRenderDevice_ = deviceDescriptor;
+    if (clientId == -1) {
+        preferredMediaRenderDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_MEDIA_RENDER;
+        preferredWithClientId.preferredMediaRenderDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_MEDIA_RENDER;
+    preferredWithClientId.preferredMediaRenderDevice_ = deviceDescriptor;
 }
 
-void AudioStateManager::SetPreferredCallRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
-{
-    preferredCallRenderDevice_ = deviceDescriptor;
-}
-
-void AudioStateManager::SetPreferredCallCaptureDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+void AudioStateManager::SetPreferredCallRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
     std::lock_guard<std::mutex> lock(mutex_);
-    preferredCallCaptureDevice_ = deviceDescriptor;
+    if (clientId == -1) {
+        preferredCallRenderDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_CALL_RENDER;
+        preferredWithClientId.preferredCallRenderDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_CALL_RENDER;
+    preferredWithClientId.preferredCallRenderDevice_ = deviceDescriptor;
 }
 
-void AudioStateManager::SetPreferredRingRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+void AudioStateManager::SetPreferredCallCaptureDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
-    preferredRingRenderDevice_ = deviceDescriptor;
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (clientId == -1) {
+        preferredCallCaptureDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_CALL_CAPTURE;
+        preferredWithClientId.preferredCallCaptureDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_CALL_CAPTURE;
+    preferredWithClientId.preferredCallCaptureDevice_ = deviceDescriptor;
 }
 
-void AudioStateManager::SetPreferredRecordCaptureDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+void AudioStateManager::SetPreferredRingRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
-    preferredRecordCaptureDevice_ = deviceDescriptor;
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (clientId == -1) {
+        preferredRingRenderDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_RING_RENDER;
+        preferredWithClientId.preferredRingRenderDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_RING_RENDER;
+    preferredWithClientId.preferredRingRenderDevice_ = deviceDescriptor;
 }
 
-void AudioStateManager::SetPreferredToneRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor)
+void AudioStateManager::SetPreferredRecordCaptureDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
-    preferredToneRenderDevice_ = deviceDescriptor;
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (clientId == -1) {
+        preferredRecordCaptureDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_RECORD_CAPTURE;
+        preferredWithClientId.preferredRecordCaptureDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_RECORD_CAPTURE;
+    preferredWithClientId.preferredRecordCaptureDevice_ = deviceDescriptor;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredMediaRenderDevice()
+void AudioStateManager::SetPreferredToneRenderDevice(const sptr<AudioDeviceDescriptor> &deviceDescriptor, int32_t clientId)
 {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (clientId == -1) {
+        preferredToneRenderDevice_ = deviceDescriptor;
+        return;
+    }
+    if (userPreferredDeviceWithUidMap_.count(clientId) == 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId;
+        userPreferredDeviceWithUidMap_[clientId] = preferredWithClientId;
+        preferredWithClientId.deviceFlag &= PREFERRED_TONE_RENDER;
+        preferredWithClientId.preferredToneRenderDevice_ = deviceDescriptor;
+    }
+    UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+    preferredWithClientId.deviceFlag &= PREFERRED_TONE_RENDER;
+    preferredWithClientId.preferredToneRenderDevice_ = deviceDescriptor;
+}
+
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredMediaRenderDevice(int32_t clientId)
+{
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_MEDIA_RENDER == PREFERRED_MEDIA_RENDER) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredMediaRenderDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredMediaRenderDevice_);
     return devDesc;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredCallRenderDevice()
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredCallRenderDevice(int32_t clientId)
 {
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_CALL_RENDER == PREFERRED_CALL_RENDER) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredCallRenderDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredCallRenderDevice_);
     return devDesc;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredCallCaptureDevice()
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredCallCaptureDevice(int32_t clientId)
 {
     std::lock_guard<std::mutex> lock(mutex_);
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_CALL_CAPTURE == PREFERRED_CALL_CAPTURE) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredCallCaptureDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredCallCaptureDevice_);
     return devDesc;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredRingRenderDevice()
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredRingRenderDevice(int32_t clientId)
 {
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_RING_RENDER == PREFERRED_RING_RENDER) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredRingRenderDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredRingRenderDevice_);
     return devDesc;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredRecordCaptureDevice()
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredRecordCaptureDevice(int32_t clientId)
 {
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_RECORD_CAPTURE == PREFERRED_RECORD_CAPTURE) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredRecordCaptureDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredRecordCaptureDevice_);
     return devDesc;
 }
 
-unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredToneRenderDevice()
+unique_ptr<AudioDeviceDescriptor> AudioStateManager::GetPreferredToneRenderDevice(int32_t clientId)
 {
+    if (clientId != -1 && userPreferredDeviceWithUidMap_.count(clientId) > 0) {
+        UserPreferredDeviceWithClientId preferredWithClientId = userPreferredDeviceWithUidMap_[clientId];
+        if (preferredWithClientId.deviceFlag & PREFERRED_TONE_RENDER == PREFERRED_TONE_RENDER) {
+            return make_unique<AudioDeviceDescriptor>(preferredWithClientId.preferredToneRenderDevice_);
+        }
+    }
     unique_ptr<AudioDeviceDescriptor> devDesc = make_unique<AudioDeviceDescriptor>(preferredToneRenderDevice_);
     return devDesc;
 }
