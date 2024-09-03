aa7243d0d131a3996afc063f8cb8ccb6e4a7ef05
LightAtom <502125925@qq.com>
2024-09-03 11:31:09 +0800

    add code
    Signed-off-by: Gisaac <gongjie13@huawei.com>

--- services/audio_service/client/src/audio_manager_proxy.cpp
+++ services/audio_service/client/src/audio_manager_proxy.cpp
@@ -1175,6 +1175,7 @@ int32_t AudioManagerProxy::UnsetOffloadMode(uint32_t sessionId)
         static_cast<uint32_t>(AudioServerInterfaceCode::UNSET_OFFLOAD_MODE), data, reply, option);
     CHECK_AND_RETURN_RET_LOG(error == ERR_NONE, error, "UnsetOffloadMode failed, error: %{public}d", error);
     return reply.ReadInt32();
+}
 
 void AudioManagerProxy::RestoreSession(const int32_t &sessionID, bool isOutput)
 {
@@ -1190,7 +1191,6 @@ void AudioManagerProxy::RestoreSession(const int32_t &sessionID, bool isOutput)
     int32_t error = Remote()->SendRequest(
         static_cast<uint32_t>(AudioServerInterfaceCode::RESTORE_SESSION), data, reply, option);
     CHECK_AND_RETURN_LOG(error == ERR_NONE, "failed, error:%{public}d", error);
-}
 }
 } // namespace AudioStandard
 } // namespace OHOS
